665d6bea55abbb2faa00763f9dab702ff67b013b
Sergio Oller <sergioller@gmail.com>
2013-03-15 00:43:03 +0100

    removed s/j/y/g
    Not used as far as I know

--- src/data/unicode2sampa.sh
+++ src/data/unicode2sampa.sh
@@ -10,6 +10,6 @@ done
 sed -e 's/á/a1/g' -e 's/é/e1/g' -e 's/ɛ́/E1/g' 's/ɔ́/O1/g' -e 's/ó/o1/g' \
     -e 's/í/i1/g' -e 's/ú/u1/g' -e 's/r/rr/g' -e 's/ɾ/r/g' \
     -e 's/ʤ/Z/g' -e 's/ʎ/L/g' -e 's/ð/D/g' -e 's/ɣ/G/g' -e 's/ʃ/S/g' \
-    -e 's/ŋ/N/g' -e 's/ɲ/J/g' -e 's/ʧ/tS/g' -e 's/j/y/g' tempA 
+    -e 's/ŋ/N/g' -e 's/ɲ/J/g' -e 's/ʧ/tS/g'  tempA 
 
 rm tempA
