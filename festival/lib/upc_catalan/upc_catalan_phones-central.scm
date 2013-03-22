fe4d7e240e6aae00a2d512da5501237b0033f7bb
Sergio Oller <sergioller@gmail.com>
2013-03-22 02:37:41 +0100

    Updated phones to both dialects

--- festival/lib/upc_catalan/upc_catalan_phones-central.scm
+++ festival/lib/upc_catalan/upc_catalan_phones-central.scm
@@ -43,7 +43,7 @@
 ;; (error "You have not yet defined a phoneset for ca (and others things ?)\n            Define it in festvox/upc_ca_mar_phoneset.scm\n")
 
 (defPhoneSet
-  upc_catalan
+  upc_catalan-central
   ;;;  Phone Features
   (;; vowel or consonant
    (vc + -)  
@@ -132,4 +132,4 @@
 )
 
 (PhoneSet.silences '(pau))
-(provide 'upc_catalan_phones)
+(provide 'upc_catalan_phones-central)
